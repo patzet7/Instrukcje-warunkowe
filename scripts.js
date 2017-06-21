b094db6d3e86ea4b52183caf93f04e0a32e2f2db
patrysia <patrycja@example.com>
2017-06-21 10:48:36 +0200

    Added files: index.html, scripts.js and style.css

---
+++ scripts.js
@@ -0,0 +1,17 @@
+﻿//Skrypt: Instukcje warunkowe w praktyce
+var a = prompt('Wartość a :'),
+	b = prompt('Wartość b :'),
+	value = ((a*a)+(2*a*b)-(b*b));
+		console.log('Wynik działania: (a*a)+(2*a*b)-(b*b)');
+
+	if (value < 0) {
+		console.log('Wartość zmiennej value jest mniejsza od 0 i wynosi:',value);
+	}
+
+	else if (value > 0) {
+		console.log('Wartość zmiennej value jest większa od 0 i wynosi: ',value);
+	}
+
+	else if (value == 0) {
+		console.log('Wartość zmiennej value wynosi 0');
+	}
